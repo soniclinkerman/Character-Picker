ff9529bda8204b1b90a0a222fa2543a28ab4f2a6
System Administrator <root@Isaiahs-MacBook-Pro.local>
2019-10-10 12:20:27 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CharacterPicker from './CharacterPicker';
+
+function App() {
+  return (
+    <div className="App">
+      <CharacterPicker/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CharacterPicker.js
@@ -0,0 +1,47 @@
+import React, {Component} from "react"
+import "./CharacterPicker.css"
+import Joker from "./Joker.jpg"
+import Marth from "./Marth.jpg"
+
+
+class  CharacterPicker extends Component{
+    constructor(props){
+        super(props);
+        this.state = {num: 0}
+        this.randomNumber = this.randomNumber.bind(this)
+    }
+    randomNumber() {
+        let rand = Math.floor(Math.random() * 2 + 1)
+        this.setState({num: rand})
+    }
+
+    render() {
+        let msg;
+
+        if (this.state.num === 1) {
+            msg = 
+            <div className="main">
+            <h2>Joker</h2>
+            <img src={Joker} className = "icon"/>  
+            </div>  
+        }
+        else if (this.state.num === 2) {
+            msg =
+            <div className="main">
+                <h2>Marth</h2>
+                <img src={Marth} className= "icon" />
+            </div>
+        }
+        return(
+            <div >
+                <h2>{msg}</h2>
+                <button onClick={this.randomNumber}>Next Character to play</button>
+                
+            </div>
+        )
+    }
+
+
+}
+
+export default CharacterPicker
